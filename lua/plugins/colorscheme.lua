a49be9f7f9a167e10bb15b512ef92b0e9c0ce1fc
rushikesh <56204964+rushin236@users.noreply.github.com>
2024-07-01 19:58:42 +0530

    Update colorscheme.lua
    changed color scheme lua file due to some error

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,16 @@
 return {
-  -- -- add gruvbox
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  -- },
-
-  -- add tokyonight
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
       style = "moon",
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
-    },
-  },
+    })
 
-  -- Configure LazyVim to load tokyonight
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
