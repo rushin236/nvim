4ff81fe442cba2dab63afb40c7d4bacb7ef2b211
rushin236 <rushikeshshindesandesh@gamil.com>
2024-09-10 02:45:35 +0530

    changed buffreline, colorscheme, treesitter-text-object

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -23,8 +23,6 @@ return {
     { "<leader>bs", "<Cmd>BufferLinePick<CR>", desc = "Selete buffers" },
     { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
   },
   opts = {
     options = {
