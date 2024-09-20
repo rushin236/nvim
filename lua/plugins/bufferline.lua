fd44982304c42c5882211c05b7c74719c18bffa9
rushin236 <rushikeshshindesandesh@gmail.com>
2024-09-21 00:05:40 +0530

    fixed issue with buffer del

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,7 +8,7 @@ return {
       config = function()
         require("bufdel").setup({
           next = "tabs",
-          quit = true,
+          quit = false, -- Prevent quitting Neovim when the last buffer is deleted
         })
       end,
     },
@@ -19,17 +19,46 @@ return {
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-    { "<leader>bd", "<cmd>BufDel<cr>", desc = "Delete Current Buffer" },
-    { "<leader>bs", "<Cmd>BufferLinePick<CR>", desc = "Selete buffers" },
+    { "<leader>bs", "<Cmd>BufferLinePick<CR>", desc = "Select buffers" },
+    { "<leader>br", "<Cmd>e!<CR>", desc = "Reload buffer" },
     { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    -- Updated <leader>bd to delete the buffer and move to the next or previous buffer
+    {
+      "<leader>bd",
+      function(n)
+        -- Delete the current buffer and move to next or previous
+        require("mini.bufremove").delete(n, false)
+
+        if vim.fn.bufnr("$") == 1 then
+          -- If it's the last buffer, open the explorer
+          vim.cmd("NvimTreeOpen")
+        else
+          -- Switch to the next or previous buffer after deletion
+          vim.cmd("BufferLineCycleNext")
+        end
+      end,
+      desc = "Delete buffer and switch",
+    },
   },
   opts = {
     options = {
-      -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      close_command = function(n)
+        local bufdelete = require("mini.bufremove").delete
+        bufdelete(n, false)
+        -- Open NvimTree if it's the last buffer
+        if vim.fn.bufnr("$") == 1 then
+          vim.cmd("NvimTreeOpen")
+        end
+      end,
+      right_mouse_command = function(n)
+        local bufdelete = require("mini.bufremove").delete
+        bufdelete(n, false)
+        -- Open NvimTree if it's the last buffer
+        if vim.fn.bufnr("$") == 1 then
+          vim.cmd("NvimTreeOpen")
+        end
+      end,
       diagnostics = "nvim_lsp",
       always_show_bufferline = true,
       offsets = {
@@ -38,7 +67,7 @@ return {
           text = "Explorer",
           highlight = "Directory",
           text_align = "left",
-          separator = true, -- use a "true" to enable the default, or set your own character
+          separator = true,
         },
       },
     },
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,28 +1,29 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  config = function()
-    local wk = require("which-key")
-    wk.add({
-      {},
-      {
-        mode = { "n", "v" },
-        { "<leader><tab>", group = "tabs" },
-        { "<leader>b", group = "buffer" },
-        { "<leader>c", group = "code" },
-        { "<leader>d", group = "diagnostics/quickfix" },
-        { "<leader>f", group = "file/find" },
-        { "<leader>g", group = "git" },
-        { "<leader>gh", group = "hunks" },
-        { "<leader>q", group = "quit/session" },
-        { "<leader>s", group = "split" },
-        { "<leader>u", group = "ui" },
-        { "<leader>w", group = "windows" },
-        { "[", group = "prev" },
-        { "]", group = "next" },
-        { "g", group = "goto" },
-        { "gs", group = "surround" },
-      },
-    })
-  end,
+  config = true,
+  -- config = function()
+  --   local wk = require("which-key")
+  --   wk.add({
+  --     {},
+  --     {
+  --       mode = { "n", "v" },
+  --       { "<leader><tab>", group = "tabs" },
+  --       { "<leader>b", group = "buffer" },
+  --       { "<leader>c", group = "code" },
+  --       { "<leader>d", group = "diagnostics/quickfix" },
+  --       { "<leader>f", group = "file/find" },
+  --       { "<leader>g", group = "git" },
+  --       { "<leader>gh", group = "hunks" },
+  --       { "<leader>q", group = "quit/session" },
+  --       { "<leader>s", group = "split" },
+  --       { "<leader>u", group = "ui" },
+  --       { "<leader>w", group = "windows" },
+  --       { "[", group = "prev" },
+  --       { "]", group = "next" },
+  --       { "g", group = "goto" },
+  --       { "gs", group = "surround" },
+  --     },
+  --   })
+  -- end,
 }
